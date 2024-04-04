e4160112600cd14b77667aa90b273c52c1a20ae3
Lex Mulier <lexmulier@AMS-OFFICE075.local>
2024-04-04 08:58:47 +0200

    Update iterm config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,5 @@
 ln -s $HOME/dotfiles/.zshrc ~/.zshrc
+
+# VS Code
+ln -sf $DOTFILES/configs/vscode-keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $DOTFILES/configs/vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
